5e4976a48694a3ecba2b22f395d897c5ca440897
Zachary Taylor <TaylorZach@prahs.com>
2016-04-03 20:18:02 -0400

    more styling and templating

--- source/js/main.js
+++ source/js/main.js
@@ -1,15 +1,57 @@
-function animateText(){
-
-	setInterval(function(){
-		
-		var active = $('span.active');
-		var spans = $('span');
-		var random = Math.floor(Math.random() * spans.length);
-		var target = spans[random];
-
-		$(active).removeClass('active');
-		$(target).addClass('active');
-
-	}, 2000);
+// jQuery to collapse the navbar on scroll
+function collapseNavbar() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
 }
-animateText();
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+  if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+    $('.navbar-toggle:visible').click();
+  }
+});
+
+// init wow.js
+wow = new WOW(
+  {
+    boxClass:     'wow',      // default
+    animateClass: 'animated', // default
+    offset:       250,          // default
+    mobile:       false,       // default
+    live:         true        // default
+  }
+)
+wow.init();
+
+// Smooth Scrolling
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
