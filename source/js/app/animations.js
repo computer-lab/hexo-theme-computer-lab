7ca339bf9aa730fd7ed680b2010403a6e36cfd87
Zachary Taylor <TaylorZach@prahs.com>
2017-01-06 16:51:22 -0500

    add projects, more design work

--- source/js/app/animations.js
+++ source/js/app/animations.js
@@ -2,13 +2,9 @@
 wow = new WOW({
       boxClass:     'wow',      // default
       animateClass: 'animated', // default
-      offset:       120,          // default
+      offset:       90,          // default
       mobile:       false,       // default
       live:         true        // default
     })
 
 wow.init();
-
-if ($('#logo-svg').length) {
-	new Vivus('logo-svg', {duration: 120});
-}
