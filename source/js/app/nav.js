366a638c759fe10ba4984f15211c7a9648ad756b
Zachary Taylor <TaylorZach@prahs.com>
2017-01-11 01:54:29 -0500

    lots of change

--- source/js/app/nav.js
+++ source/js/app/nav.js
@@ -1,30 +1,30 @@
+//jQuery to collapse the navbar on scroll
+function collapseNavbar() {
+
+if ($(".navbar").offset().top > 50) {
+  $(".navbar-fixed-top").addClass("top-nav-collapse");
+} else {
+  $(".navbar-fixed-top").removeClass("top-nav-collapse");
+}
+}
+
 // jQuery to collapse the navbar on scroll
-// function collapseNavbar() {
+function collapseNavbar() {
 
-// if ($(".navbar").offset().top > 50) {
-//   $(".navbar-fixed-top").addClass("top-nav-collapse");
-// } else {
-//   $(".navbar-fixed-top").removeClass("top-nav-collapse");
-// }
-// }
+  if ($(".navbar").offset().top > 50) {
+    $(".navbar-fixed-top").addClass("top-nav-collapse");
+  } else {
+    $(".navbar-fixed-top").removeClass("top-nav-collapse");
+  }
+}
 
-// // jQuery to collapse the navbar on scroll
-// function collapseNavbar() {
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
 
-//   if ($(".navbar").offset().top > 50) {
-//     $(".navbar-fixed-top").addClass("top-nav-collapse");
-//   } else {
-//     $(".navbar-fixed-top").removeClass("top-nav-collapse");
-//   }
-// }
-
-// $(window).scroll(collapseNavbar);
-// $(document).ready(collapseNavbar);
-
-// // Closes the Responsive Menu on Menu Item Click
-// $('.navbar-collapse ul li a').click(function() {
-//   if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
-//     $('.navbar-toggle:visible').click();
-//   }
-// });
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+  if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+    $('.navbar-toggle:visible').click();
+  }
+});
 
